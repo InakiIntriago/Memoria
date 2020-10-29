adba5d0078742c126a3e78ac584894727434b32e
InakiIntriago <73494944+InakiIntriago@users.noreply.github.com>
2020-10-29 08:22:02 -0600

    Create Memoria.py
    Este programa intenta hacer que los números queden centrados en los cuadros y que el número de taps se muestre en la pantalla.

---
+++ Memoria.py
@@ -0,0 +1,79 @@
+from random import *
+from turtle import *
+from freegames import path
+
+car = path('car.gif')
+tiles = list(range(32)) * 2
+writer = Turtle(visible=False)
+state = {'mark': None}
+hide = [True] * 64
+
+def square(x, y):
+    "Draw white square with black outline at (x, y)."
+    up()
+    goto(x, y)
+    down()
+    color('black', 'white')
+    begin_fill()
+    for count in range(4):
+        forward(50)
+        left(90)
+    end_fill()
+
+def index(x, y):
+    "Convert (x, y) coordinates to tiles index."
+    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+
+def xy(count):
+    "Convert tiles count to (x, y) coordinates."
+    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+
+def tap(x, y):
+    writer.undo()
+    writer.write(state['mark'])
+    "Update mark and hidden tiles based on tap."
+    spot = index(x, y)
+    mark = state['mark']
+
+    if mark is None or mark == spot or tiles[mark] != tiles[spot]:
+        state['mark'] = spot
+    else:
+        hide[spot] = False
+        hide[mark] = False
+        state['mark'] = None
+
+def draw():
+    "Draw image and tiles."
+    clear()
+    goto(0, 0)
+    shape(car)
+    stamp()
+
+    for count in range(64):
+        if hide[count]:
+            x, y = xy(count)
+            square(x, y)
+
+    mark = state['mark']
+
+    if mark is not None and hide[mark]:
+        x, y = xy(mark)
+        up()
+        goto(x + 10, y + 5)
+        color('black')
+        write(tiles[mark], font=('Arial', 30, 'normal'))
+
+    update()
+    ontimer(draw, 100)
+
+shuffle(tiles)
+setup(420, 420, 370, 0)
+addshape(car)
+hideturtle()
+tracer(False)
+writer.goto(230, 180)
+writer.color('black')
+writer.write(state['mark'])
+onscreenclick(tap)
+draw()
+done()
